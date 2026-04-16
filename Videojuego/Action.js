a1ee8c80f5e41e0dea38acfc3e3a2e3c4b0bf2f1
Juan Pablo Briñez <A01782050@tec.mx>
2026-04-16 13:15:03 -0600

    Actualizacion estrcutura de clases

---
+++ Videojuego/Action.js
@@ -0,0 +1,11 @@
+class action {
+    constructor(name, description, staminaCost, actionType, effect, attribute, target) {
+        this.name = name;
+        this.description = description;
+        this.staminaCost = staminaCost;
+        this.actionType = actionType;
+        this.effect = effect;
+        this.attribute = attribute;
+        this.target = target;
+    }
+}
--- Videojuego/ItemCard.js
+++ Videojuego/ItemCard.js
@@ -1,14 +1,21 @@
-class ItemCard extends GameObject{
-    
-    constructor() {
-        this.name = "";
-        this.description = "";
-        this.image = null;
-    }
+import { attributes } from "./GlobalVariables"; 
 
-    constructor(name, description, image) {
+class ItemCard extends GameObject{
+
+    constructor(name, description, image, statistics, requirements, 
+                rarity, staminaCost, actionType, action) {
         this.name = name;
         this.description = description;
         this.image = image;
+        this.stats = statistics;
+        this.requirements = requirements;
+        this.rarity = rarity;
+        this.staminaCost = staminaCost;
+        this.actionType = actionType;
+        this.action = action;
     }
+
+    
+
+
 }
