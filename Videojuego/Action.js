66b3926da17e1dc8a3c200f70a718640d38ce594
Juan Pablo Briñez <A01782050@tec.mx>
2026-04-17 14:46:54 -0600

    Action & Character classes finished

--- Videojuego/Action.js
+++ Videojuego/Action.js
@@ -1,11 +1,49 @@
-class action {
-    constructor(name, description, staminaCost, actionType, effect, attribute, target) {
+import { ACTION_TYPES, BASE_ATTRIBUTES } from "./GlobalVariables";
+
+export default class Action {
+    constructor(
+        name = "", 
+        description = "", 
+        actionType = ACTION_TYPES.ATTACK,
+        staminaCost = 0, 
+        baseDamage = 0, 
+        baseDefense = 0,
+        baseMagicDamage = 0,
+        baseMagicDefense = 0,
+        scalingAttribute = BASE_ATTRIBUTES.STRENGTH, 
+        scaleFactor = 1.0,
+        target = null
+    ) {
         this.name = name;
         this.description = description;
-        this.staminaCost = staminaCost;
         this.actionType = actionType;
-        this.effect = effect;
-        this.attribute = attribute;
+        this.staminaCost = staminaCost;
+        this.baseDamage = baseDamage;
+        this.baseDefense = baseDefense;
+        this.baseMagicDamage = baseMagicDamage;
+        this.baseMagicDefense = baseMagicDefense;
+        this.scalingAttribute = scalingAttribute;
+        this.scaleFactor = scaleFactor;
         this.target = target;
+    }
+
+    calculateDamage(attributes = {}) {
+        const attributeValue = attributes[this.scalingAttribute] ?? 0;
+        return this.baseDamage + (attributeValue * this.scaleFactor);
+    }
+
+    calculateDefense(attributes = {}) {
+        const attributeValue = attributes[this.scalingAttribute] ?? 0;
+        return this.baseDefense + (attributeValue * this.scaleFactor);
+    }
+
+    calculateMagicDamage(attributes = {}) {
+        const attributeValue = attributes[this.scalingAttribute] ?? 0;
+        return this.baseMagicDamage + (attributeValue * this.scaleFactor);
+    }
+
+    calculateMagicDefense(attributes = {}) {
+        const attributeValue = attributes[this.scalingAttribute] ?? 0;
+        return this.baseMagicDefense + (attributeValue * this.scaleFactor);
     }
 }
