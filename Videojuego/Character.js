c4cccac74471270723410fe7f22fc81d9f7046c5
Juan Pablo Briñez <A01782050@tec.mx>
2026-04-17 14:46:54 -0600

    Action & Character classes finished

--- Videojuego/Character.js
+++ Videojuego/Character.js
@@ -3,10 +3,55 @@ import {BASE_ATTRIBUTES} from "./GlobalVariables";
 
 
 export default class Character extends GameObject {
-    constructor(name, health, stamina, attributes) {
+    constructor(
+        name, 
+        maxHealth = 100, 
+        health = maxHealth, 
+        maxStamina = 100, 
+        stamina = maxStamina, 
+        attributes = {...BASE_ATTRIBUTES},
+        x = 0,
+        y = 0,
+        width = 0,
+        height = 0
+     ) {
         this.name = name;
+        this.maxHealth = maxHealth;
         this.health = health;
+        this.maxStamina = maxStamina;
         this.stamina = stamina;
-        this.attributes = attributes;
+        this.attributes = {...BASE_ATTRIBUTES, ...attributes};
+        super(x, y, width, height);
     }
+
+    isAlive() {
+        if (this.health > 0) {
+            return true;
+        } 
+        else {
+            return false;
+        }  
+    }
+
+    takesDamage(damage) {
+        this.health -= damage;
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    }
+
+    spendStamina(amount) {
+        this.stamina -= amount;
+        if (this.stamina < 0) {
+            this.stamina = 0;
+        }
+    }
+
+    recoverStamina(amount) {
+        this.stamina += amount;
+        if (this.stamina > this.maxStamina) {
+            this.stamina = this.maxStamina;
+        }
+    }
+
 }
