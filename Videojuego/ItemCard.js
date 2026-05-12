c0319d209e43b65d0de9f89ba7cf8f0c2767ba1b
Juan Pablo Briñez <A01782050@tec.mx>
2026-05-12 13:03:19 -0600

    Clases

--- Videojuego/ItemCard.js
+++ Videojuego/ItemCard.js
@@ -1,25 +1,23 @@
-import GameObject from "./GameObject";
-import {BASE_ATTRIBUTES, CARD_RARITY, ACTION_TYPES} from "./GlobalVariables"; 
-import Action from "./Action";
+import GameObject from "./GameObject.js";
+import { CARD_RARITY, ACTION_TYPES } from "./GlobalVariables.js";
+import Action from "./Action.js";
 
-export default class ItemCard extends GameObject{
-
+export default class ItemCard extends GameObject {
     constructor(
-        name = "", 
-        description = "", 
-        image = null, 
-        actions = new Action(), 
-        requirements = {}, 
-        rarity = CARD_RARITY.COMMON, 
-        staminaCost = 0, 
+        name = "",
+        description = "",
+        image = null,
+        action = new Action(),
+        requirements = {},
+        rarity = CARD_RARITY.COMMON,
+        staminaCost = 0,
         isPermanent = false
-        
     ) {
         super();
         this.name = name;
         this.description = description;
         this.image = image;
-        this.actions = actions;
+        this.action = action;
         this.requirements = requirements;
         this.rarity = rarity;
         this.staminaCost = staminaCost;
@@ -33,21 +31,18 @@ export default class ItemCard extends GameObject{
         return true;
     }
 
-     calculateEffect(playerAttributes = {}) {
-        if (ACTION_TYPES.ATTACK_PHYSIC) {
-            return this.Action.calculateDamage(playerAttributes);
-        }
-        else if (ACTION_TYPES.ATTACK_MAGIC) {
-            return this.Action.calculateMagicDamage(playerAttributes);
-        }
-        else if (ACTION_TYPES.DEFEND_PHYSIC) {
-            return this.Action.calculateDefense(playerAttributes);
-        }
-        else if (ACTION_TYPES.DEFEND_MAGIC) {
-            return this.Action.calculateMagicDefense(playerAttributes);
-        }
-        else {
-            return false;
+    calculateEffect(playerAttributes = {}) {
+        switch (this.action.actionType) {
+            case ACTION_TYPES.ATTACK_PHYSIC:
+                return this.action.calculateDamage(playerAttributes);
+            case ACTION_TYPES.ATTACK_MAGIC:
+                return this.action.calculateMagicDamage(playerAttributes);
+            case ACTION_TYPES.DEFEND_PHYSIC:
+                return this.action.calculateDefense(playerAttributes);
+            case ACTION_TYPES.DEFEND_MAGIC:
+                return this.action.calculateMagicDefense(playerAttributes);
+            default:
+                return 0;
         }
     }
 }
--- Videojuego/NonPlayableCharacter.js
+++ Videojuego/NonPlayableCharacter.js
@@ -1,7 +1,7 @@
-import { Character } from "./Character.js";
-import {BASE_ATTRIBUTES} from "./GlobalVariables";
+import Character from "./Character.js";
+import { ACTION_TYPES } from "./GlobalVariables.js";
 
-export default class NPC extends Character {
+export class NPC extends Character {
     constructor(name = "", dialogue = []) {
         super(name);
         this.dialogue = dialogue;
@@ -9,30 +9,30 @@ export default class NPC extends Character {
     }
 
     getNextDialogue() {
-        if (this.dialogueIndex >= this.dialogues.length) return "";
-        return this.dialogues[this.dialogueIndex++];
+        if (this.dialogueIndex >= this.dialogue.length) return "";
+        return this.dialogue[this.dialogueIndex++];
     }
- 
+
     resetDialogue() {
         this.dialogueIndex = 0;
     }
 }
 
-export default class Enemy extends Character {
+export class Enemy extends Character {
     constructor(
         name = "",
-        health = 100, 
+        health = 100,
         stamina = 50,
-        attributes = {...BASE_ATTRIBUTES},
-        physicalDamage = 10, 
-        magicDamage = 10, 
+        attributes = {},
+        physicalDamage = 10,
+        magicDamage = 10,
         physicalDefense = 5,
-        magicDefense = 5, 
+        magicDefense = 5,
         experienceReward = 50,
         cardReward = null,
         isBoss = false
     ) {
-        super(name, health, stamina, {...BASE_ATTRIBUTES, ...attributes}); 
+        super(name, health, health, stamina, stamina, attributes);
         this.physicalDamage = physicalDamage;
         this.magicDamage = magicDamage;
         this.physicalDefense = physicalDefense;
@@ -65,61 +65,58 @@ export default class Enemy extends Character {
             state = "defensive";
         }
 
-        // =========================
-        // DECISION MAKING
-        // =========================
+        // Opciones disponibles según stats
+        const options = [];
+        if (this.physicalDamage > 0) options.push(ACTION_TYPES.ATTACK_PHYSIC);
+        if (this.magicDamage > 0) options.push(ACTION_TYPES.ATTACK_MAGIC);
+        if (this.physicalDefense > 0) options.push(ACTION_TYPES.DEFEND_PHYSIC);
+        if (this.magicDefense > 0) options.push(ACTION_TYPES.DEFEND_MAGIC);
 
-        if (Math.random() < 0.2) {
-            return randomOption;
+        // 20% de probabilidad de tomar una acción aleatoria entre las viables
+        if (Math.random() < 0.2 && options.length > 0) {
+            return options[Math.floor(Math.random() * options.length)];
         }
 
         // DEFENSIVE STATE
         if (state === "defensive") {
             if (playerType === "physical" && this.physicalDefense > 0) {
-                return ACTIONS.DEFEND_PHYSIC;
+                return ACTION_TYPES.DEFEND_PHYSIC;
             }
             if (playerType === "magic" && this.magicDefense > 0) {
-                return ACTIONS.DEFEND_MAGIC;
+                return ACTION_TYPES.DEFEND_MAGIC;
             }
         }
 
         // AGGRESSIVE STATE
         if (state === "aggressive") {
             if (this.physicalDamage > this.magicDamage && this.physicalDamage > 0) {
-                return ACTIONS.ATTACK_PHYSIC;
+                return ACTION_TYPES.ATTACK_PHYSIC;
             }
             if (this.magicDamage > this.physicalDamage && this.magicDamage > 0) {
-                return ACTIONS.ATTACK_MAGIC;
+                return ACTION_TYPES.ATTACK_MAGIC;
             }
         }
 
         // NEUTRAL / BALANCED
         if (playerType === "physical") {
-            return this.physicalDefense > 0 
-                ? ACTIONS.DEFEND_PHYSIC
-                : ACTIONS.ATTACK_MAGIC;
+            return this.physicalDefense > 0
+                ? ACTION_TYPES.DEFEND_PHYSIC
+                : ACTION_TYPES.ATTACK_MAGIC;
         }
 
         if (playerType === "magic") {
-            return this.magicDefense > 0 
-                ? ACTIONS.DEFEND_MAGIC 
-                : ACTIONS.ATTACK_PHYSICAL;
+            return this.magicDefense > 0
+                ? ACTION_TYPES.DEFEND_MAGIC
+                : ACTION_TYPES.ATTACK_PHYSIC;
         }
 
         // fallback (jugador balanceado)
-        const options = [];
-
-        if (this.physicalDamage > 0) options.push(ACTIONS.ATTACK_PHYSICAL);
-        if (this.magicDamage > 0) options.push(ACTIONS.ATTACK_MAGIC);
-        if (this.physicalDefense > 0) options.push(ACTIONS.DEFEND_PHYSICAL);
-        if (this.magicDefense > 0) options.push(ACTIONS.DEFEND_MAGIC);
-
         return options[Math.floor(Math.random() * options.length)];
     }
 
-    bossRewards(isBoss) {
-        if (isBoss) {
+    bossRewards() {
+        if (this.isBoss) {
             this.experienceReward *= 3;
         }
-    }   
+    }
 }
