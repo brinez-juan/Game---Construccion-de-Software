c65396ff9d96dc91bcfb959a1551436e85484e79
Santiago Martin <a01787450@tec.mx>
2026-05-17 11:34:39 -0600

    Added class and methods for credit screen

--- Videojuego/Menus.js
+++ Videojuego/Menus.js
@@ -1,5 +1,5 @@
 "use strict"; 
-
+//Move each of the classes to an independent file to make a cleaner structure
 //Change Menus name to screens because it will be the base class for all the screens in the game
 class Menus{
     constructor(background = '', canvasWidth = 0, canvasHeight = 0){
@@ -93,6 +93,8 @@ class mainMenu extends Menus{
     }
 }
 
+
+//You guys can modify ui aspects if you want to but ensure UI keeps clean after
 class selectionMenu extends Menus{
     constructor(background = '', canvasWidth = 0, canvasHeight = 0, playerData, menu){
         super(background, canvasWidth, canvasHeight)
@@ -277,5 +279,44 @@ class selectionMenu extends Menus{
                 }
             }
         })
+    }
+}
+
+class creditScreen extends Menus{
+    constructor(background = '', canvasWidth = 0, canvasHeight = 0){
+        super(background, canvasWidth, canvasHeight)
+        this.returnButton = this.returnButton = new GameObject(this.canvasWidth/11,this.canvasHeight/11, 150, 65, undefined, true, true)
+        this.returnButton.setSprite('../Assets/Sprites/return_button.png')
+    }
+
+    initElements(){
+        canvas.addEventListener('mousemove', (e)=>{
+            const rect = canvas.getBoundingClientRect();
+            const mouseX = e.clientX - rect.left;
+            const mouseY = e.clientY - rect.top;
+            this.returnButton.mouseCollition(mouseX, mouseY)
+        })
+
+        canvas.addEventListener('click', (e) =>{
+            if(this.returnButton.hovered){
+                //Change this when adding states
+                this.state = 0
+                console.log('sex')
+            }
+        })
+    }
+
+    draw(ctx){
+        this.background.draw(ctx)
+        this.returnButton.draw(ctx)
+    }
+
+    update(deltaTime){
+        if(this.returnButton.hovered){
+            this.returnButton.setSprite('../Assets/Sprites/return_2.png')
+        }
+        else{
+            this.returnButton.setSprite('../Assets/Sprites/return_button.png')
+        }
     }
 }
