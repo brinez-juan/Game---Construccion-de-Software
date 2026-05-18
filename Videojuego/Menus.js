aa8200b2c28757688ee160dca9e61bc73702e5c3
Santiago Martin <a01787450@tec.mx>
2026-05-17 21:01:06 -0600

    Created options menu and added some basic ui elements

--- Videojuego/Menus.js
+++ Videojuego/Menus.js
@@ -347,3 +347,73 @@ class loadingScreen extends Menus{
         }
     }
 }
+
+class optionsMenu extends Menus{
+    constructor(background = '', canvasWidth = 0, canvasHeight = 0, menuType){
+        super(background, canvasWidth, canvasHeight)
+        this.type = menuType 
+        this.volumeMusic;
+        this.volumeSFX; 
+        this.initElements(); 
+    }
+
+    initElements(){
+        this.generalVolumeField = {text: '', bar: '', slideElement: ''};
+        this.returnButton = new GameObject(this.canvasWidth/10,this.canvasHeight/7, 150, 65, undefined, true, true)
+        this.returnButton.setSprite('../Assets/Sprites/return_button.png')
+        this.sfxField = new GameObject(this.canvasWidth/4, 2*this.canvasHeight/4, 180, 80, 'on', undefined, undefined);
+        this.soundField = new GameObject(this.canvasWidth/4, 3*this.canvasHeight/4, 180, 80, 'on', undefined, undefined);
+        this.sfxField.setSprite('../Assets/Sprites/sfx_on.png')
+        this.soundField.setSprite('../Assets/Sprites/music_on.png')
+        canvas.addEventListener('mousemove', (e)=>{
+            const rect = canvas.getBoundingClientRect();
+            const mouseX = e.clientX - rect.left;
+            const mouseY = e.clientY - rect.top;
+            this.returnButton.mouseCollition(mouseX, mouseY)
+            this.sfxField.mouseCollition(mouseX, mouseY)
+            this.soundField.mouseCollition(mouseX, mouseY)
+        })
+
+        canvas.addEventListener('click', (e) =>{
+            if(this.returnButton.hovered){
+                //Change this when adding states
+                this.state = 0
+            }
+            this.fieldChecker()
+        })
+    }
+
+    draw(ctx){
+        this.background.draw(ctx)
+        this.sfxField.draw(ctx)
+        this.soundField.draw(ctx)
+        this.returnButton.draw(ctx)
+    }
+
+    update(deltaTime){
+
+    }
+
+    fieldChecker(){
+        if(this.sfxField.hovered && this.sfxField.type === 'on'){
+                this.sfxField.setSprite('../Assets/Sprites/sfx_off.png')
+                this.sfxField.type = 'off'
+                //Add logic to set volume
+        }
+        else if(this.sfxField.hovered){
+            this.sfxField.setSprite('../Assets/Sprites/sfx_on.png')
+            this.sfxField.type = 'on'
+            //Add logic to set volume
+        }
+        if(this.soundField.hovered && this.soundField.type === 'on'){
+                this.soundField.setSprite('../Assets/Sprites/music_off.png')
+                this.soundField.type = 'off'
+                //Add logic to set volume
+        }
+        else if(this.soundField.hovered){
+            this.soundField.setSprite('../Assets/Sprites/music_on.png')
+            this.soundField.type = 'on'
+            //Add logic to set volume
+        }
+    }
+}
