f6cbc274bbbfbac8079e3210efcf750c96304d98
Santiago Martin <a01787450@tec.mx>
2026-05-21 23:53:39 -0600

    Added main mechanics for turns and parry bar

--- Videojuego/Game/NonPlayableCharacter.js
+++ Videojuego/Game/NonPlayableCharacter.js
@@ -54,15 +54,6 @@ export class Enemy extends Character {
         this.indicator.setSprite('')
     }
 
-    addEventListeners() {
-        canvas.addEventListener('mousemove', (e) => {
-            const rect = canvas.getBoundingClientRect();
-            const mouseX = e.clientX - rect.left;
-            const mouseY = e.clientY - rect.top;
-            this.mouseCollition(mouseX, mouseY)
-        });
-    }
-
     removeEventListeners() {
         canvas.removeEventListener('mousemove', this.mouseCollition);
     }
