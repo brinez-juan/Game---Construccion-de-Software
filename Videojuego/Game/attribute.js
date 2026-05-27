4a6b67e6761e706d424d3279e64b649fe6d2ce76
Santiago Martin <a01787450@tec.mx>
2026-05-26 19:05:04 -0600

    Implemented attributes in the UI of the battle lobby

--- Videojuego/Game/attribute.js
+++ Videojuego/Game/attribute.js
@@ -2,12 +2,17 @@ import textLabel from "./textLabel.js";
 import gameObject from "./GameObject.js";
 
 export default class Attribute{
-    constructor(posX, posY, size, attributeName, attributeValue, deck, inventory){
-        this.posX = posX
-        this.posY = posY
-        this.size = size
-        this.attributeName = attributeName
-        this.attributeLabel = new textLabel(posX, posY, `${size}px Academia`, 'black', undefined, attributeName, false)
-        this.attributeValueLabel = new textLabel(posX, posY, `${size}px Academia`, 'black', undefined, attributeValue, false)
+    constructor(posX, posY, size, attributeName, attributeValue){
+        this.name = attributeName
+        this.additionButton = new gameObject(posX, posY-7, size, size)
+        this.additionButton.setSprite('../Assets/Sprites/addition_button.png')
+        this.attributeLabel = new textLabel(posX - 120, posY, `${size}px Academia`, 'black', undefined, attributeName, false)
+        this.attributeValueLabel = new textLabel(posX - 50, posY, `${size}px Academia`, 'black', undefined, attributeValue, false)
+    }
+
+    draw(ctx){
+        this.additionButton.draw(ctx)
+        this.attributeLabel.draw(ctx)
+        this.attributeValueLabel.draw(ctx)
     }
 }
--- Videojuego/Game/battleLobby.js
+++ Videojuego/Game/battleLobby.js
@@ -2,12 +2,17 @@ import Menus from "./Menus.js";
 import Bar from "./Bar.js";
 import itemCard from "./ItemCard.js";
 import textLabel from "./textLabel.js";
+import Attribute from "./attribute.js";
+
+// Lobby menu displayed between battles to show player progression and allow attribute upgrades
 
 export default class battleLobby extends Menus{
-    constructor(background = '', canvasWidth = 0, canvasHeight = 0,experienceToNextLevel,  experience, level){
+    constructor(background = '', canvasWidth = 0, canvasHeight = 0,experienceToNextLevel,  experience, level, attributes){
         super(background, canvasWidth, canvasHeight)
         this.experienceBarElements = []; 
+        this.attributeElements = []; 
         this.experienceBarSpawn(experience, level, experienceToNextLevel)
+        this.attributeSectionSpaw(attributes)
     }
 
     experienceBarSpawn(exp, level, expToNextLevel){
@@ -15,7 +20,7 @@ export default class battleLobby extends Menus{
         let offSetX = 40; 
         let initialPosX = this.canvasWidth/5*4 - offSetX
         let initialPosY = this.canvasHeight/5 - offSetY
-        let expLabel = new textLabel(initialPosX, initialPosY, '20px Academia', 'black', undefined, 'Experience', false)
+        let expLabel = new textLabel(initialPosX, initialPosY, '30px Academia', 'black', undefined, 'Experience', false)
         this.experienceBarElements.push(expLabel)
         initialPosY += offSetY
         let expBar = new Bar(initialPosX, initialPosY, 250, 20, '../Assets/Sprites/stamina_bar.png', expToNextLevel)
@@ -29,10 +34,29 @@ export default class battleLobby extends Menus{
         let levelLabel = new textLabel(initialPosX, initialPosY, '20px Academia', 'black', undefined, `Level ${level}`, false)
         this.experienceBarElements.push(levelLabel)
     }
+
+    attributeSectionSpaw(attributes){
+        let offSetY = 30;
+        let offsetX = 40;
+        let initialPosX = this.canvasWidth/5*4 - offsetX
+        let initialPosY = this.canvasHeight/2
+        let attributeSectionLabel = new textLabel(initialPosX, initialPosY, '30px Academia', 'black', undefined, 'Attributes', false)
+        this.attributeElements.push(attributeSectionLabel)
+        initialPosX += 2*offsetX
+        initialPosY += offSetY/2
+        for(const [key, value] of Object.entries(attributes)){
+            initialPosY += offSetY
+            let attribute = new Attribute(initialPosX, initialPosY, 20, key, value)
+            this.attributeElements.push(attribute)
+        }
+    }
     draw(ctx){
         this.background.draw(ctx)
         for(let element of this.experienceBarElements){
             element.draw(ctx)
+        }
+        for(let attribute of this.attributeElements){
+            attribute.draw(ctx)
         }
     }
 }
